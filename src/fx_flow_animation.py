3af21e616d4a9c3cb895a149ce75e67902208fa8
Youngik-Lee <youngik_lee@alumni.brown.edu>
2025-11-28 15:07:06 +0900

    Create fx_flow_animation.py

---
+++ src/fx_flow_animation.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import imageio
+from fx_flow_model import build_country_graph, compute_flows
+from fx_flow_api import fetch_rates_real
+
+def draw_frame(G, flow, pos, filename):
+    plt.figure(figsize=(10,7))
+    nx.draw_networkx_nodes(G,pos,node_size=1500,node_color=(flow-1),cmap="coolwarm")
+    nx.draw_networkx_labels(G,pos)
+
+    for u,v in G.edges():
+        fu = flow[CURRENCIES.index(u)]
+        fv = flow[CURRENCIES.index(v)]
+        if fu < fv:
+            start,end=u,v; mag=fv-fu
+        else:
+            start,end=v,u; mag=fu-fv
+
+        nx.draw_networkx_edges(
+            G,pos,edgelist=[(start,end)],width=3*mag,
+            arrowstyle="->",arrowsize=30
+        )
+    plt.axis("off")
+    plt.savefig(filename)
+    plt.close()
+
+if __name__ == "__main__":
+    G = build_country_graph()
+    rates = fetch_rates_real(CURRENCIES)
+    flows = compute_flows(rates)
+
+    pos = nx.spring_layout(G, seed=2)
+
+    frames=[]
+    for i in range(len(flows)):
+        f = flows.iloc[i].values
+        fname = f"frame_{i}.png"
+        draw_frame(G,f,pos,fname)
+        frames.append(imageio.imread(fname))
+
+    imageio.mimsave("flow_animation.gif", frames, fps=2)
+    print("Saved animation: flow_animation.gif")
